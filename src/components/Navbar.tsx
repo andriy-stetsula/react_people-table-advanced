11724b8e45fad074fa8a685caa202244729389d9
Андрій <andriystetsula2344gmail.com@MacBook-Pro-Andrij.local>
2026-06-22 02:05:47 +0300

    solution

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from 'react-router-dom';
+
 export const Navbar = () => {
   return (
     <nav
@@ -8,17 +10,25 @@ export const Navbar = () => {
     >
       <div className="container">
         <div className="navbar-brand">
-          <a className="navbar-item" href="#/">
+          <NavLink
+            end
+            className={({ isActive }) =>
+              `navbar-item ${isActive ? 'has-background-grey-lighter' : ''}`
+            }
+            to="/"
+          >
             Home
-          </a>
+          </NavLink>
 
-          <a
+          <NavLink
             aria-current="page"
-            className="navbar-item has-background-grey-lighter"
-            href="#/people"
+            className={({ isActive }) =>
+              `navbar-item ${isActive ? 'has-background-grey-lighter' : ''}`
+            }
+            to="/people"
           >
             People
-          </a>
+          </NavLink>
         </div>
       </div>
     </nav>
--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,29 +1,103 @@
 import { PeopleFilters } from './PeopleFilters';
 import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { getPeople } from '../api';
+import { Person } from '../types';
 
 export const PeoplePage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+  const [people, setPeople] = useState<Person[]>([]);
+
+  useEffect(() => {
+    getPeople()
+      .then(setPeople)
+      .catch(() => setHasError(true))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const [searchParams] = useSearchParams();
+
+  const sort = searchParams.get('sort');
+  const order = searchParams.get('order');
+
+  const query = searchParams.get('query') || '';
+  const sex = searchParams.get('sex') || '';
+  const centuries = searchParams.getAll('centuries');
+
+  const filteredPeople = people.filter(person => {
+    const normalizedQuery = query.toLowerCase();
+
+    const matchesQuery =
+      person.name.toLowerCase().includes(normalizedQuery) ||
+      (person.motherName || '').toLowerCase().includes(normalizedQuery) ||
+      (person.fatherName || '').toLowerCase().includes(normalizedQuery);
+
+    const matchesSex = !sex || person.sex === sex;
+
+    const century = Math.ceil(person.born / 100).toString();
+    const matchesCentury =
+      centuries.length === 0 || centuries.includes(century);
+
+    return matchesQuery && matchesSex && matchesCentury;
+  });
+
+  const visiblePeople = [...filteredPeople].sort((a, b) => {
+    switch (sort) {
+      case 'name':
+      case 'sex':
+        return a[sort].localeCompare(b[sort]);
+
+      case 'born':
+      case 'died':
+        return a[sort] - b[sort];
+      default:
+        return 0;
+    }
+  });
+
+  if (order === 'desc') {
+    visiblePeople.reverse();
+  }
+
+  const hasNoMatches =
+    !isLoading && !hasError && people.length > 0 && visiblePeople.length === 0;
+
   return (
     <>
       <h1 className="title">People Page</h1>
 
       <div className="block">
         <div className="columns is-desktop is-flex-direction-row-reverse">
-          <div className="column is-7-tablet is-narrow-desktop">
-            <PeopleFilters />
-          </div>
+          {people.length > 0 && (
+            <div className="column is-7-tablet is-narrow-desktop">
+              <PeopleFilters />
+            </div>
+          )}
 
           <div className="column">
             <div className="box table-container">
-              <Loader />
+              {isLoading && <Loader />}
 
-              <p data-cy="peopleLoadingError">Something went wrong</p>
+              {hasError && (
+                <p data-cy="peopleLoadingError">Something went wrong</p>
+              )}
 
-              <p data-cy="noPeopleMessage">There are no people on the server</p>
+              {!isLoading && !hasError && people.length === 0 && (
+                <p data-cy="noPeopleMessage">
+                  There are no people on the server
+                </p>
+              )}
 
-              <p>There are no people matching the current search criteria</p>
+              {hasNoMatches && (
+                <p>There are no people matching the current search criteria</p>
+              )}
 
-              <PeopleTable />
+              {visiblePeople.length > 0 && (
+                <PeopleTable people={visiblePeople} />
+              )}
             </div>
           </div>
         </div>
